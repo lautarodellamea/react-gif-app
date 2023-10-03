bc70b564b13671f0a0cb4e490135613fedd99414
Lautaro Della Mea <Lautarodm98@gmail.com>
2023-10-03 12:37:27 -0300

    style changes

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -2,9 +2,9 @@
 
 export const GifItem = ({ title, url, id }) => {
   return (
-    <div className="bg-slate-800 rounded-md">
+    <div className="bg-slate-800 rounded-md ">
       <img className="h-[250px] w-auto" src={url} alt={title} />
-      <p className="text-white p-2 w-[150px] text-center mx-auto">{title}</p>
+      <p className="text-white p-2 w-[150px] text-center mx-auto ">{title}</p>
     </div>
   );
 };
